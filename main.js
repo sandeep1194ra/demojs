53361b568918f018fabf87e0c4be1804b0fc7c24
sandeep1194ra <sandeep01022024@gmail.com>
2025-04-03 11:32:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ document.getElementById("demo").innerHTML = "You have changed paragraph";
 }
 
 function date() {
-document.getElementById("demo1").innerHTML. = Date();
+document.getElementById("demo1").innerHTML = Date();
 }
 
+function demoAlert() {
+document.getElementById("demo2").innerHTML = alert("Do you like our Pages?");
+}
