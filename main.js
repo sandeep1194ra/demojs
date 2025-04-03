c75912911b5d8e0eacda3725159b48d3e9f294b1
sandeep1194ra <sandeep01022024@gmail.com>
2025-04-03 11:48:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 function changePara() {
 document.getElementById("demo").innerHTML = "You have changed paragraph";
+document.getElementById("demo").innerHTML = "<h1>You Changed Paragraph to Heading<h1>";
 }
 
 function date() {
 document.getElementById("demo1").innerHTML = Date();
+document.getElementById("demo1").style.fontweight="800";
 }
 
 function demoAlert() {
 document.getElementById("demo2").innerHTML = alert("Do you like our Pages?");
+document.getElementById("demo2").innerHTML = "You liked our Page.";
 }
