b4a4dbc60e5d8c2593a1095647f78cc29ff08a5a
sandeep1194ra <sandeep01022024@gmail.com>
2025-04-08 16:46:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,2 @@
-const x = document.getElementById("demo1");
-const y = document.getElementById("demo2");
-const z = document.getElementById("demo3");
 
-function text123() {
-x.innerHTML = "Hello Guys";
-y.innerText = "What you Say";
-x.style.color = "red";
-}
-
-function text234() {
-y.style.background-color = "gold";
-
-}
-
-setTimeout(text123, 5000);
-setTimeout(text234, 10000);
 
