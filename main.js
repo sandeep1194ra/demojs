3d24793a9526d33f4f7d0a50868e5d2b91262e08
sandeep1194ra <sandeep01022024@gmail.com>
2025-04-08 16:57:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
+let x = document.getElementById("demo1").innerHTML = "Welcome to Our Webpage";
+y = document.getElementById("demo2").innerText = "You can find everything here";
 
-
+setTimeout(x, 2000);
+setTimeout(y, 4000);
