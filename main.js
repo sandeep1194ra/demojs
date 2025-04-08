b0aa4f7b7273a937b4d7dc586cf175f775db09ec
sandeep1194ra <sandeep01022024@gmail.com>
2025-04-08 16:37:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ const x = document.getElementById("demo1");
 const y = document.getElementById("demo2");
 const z = document.getElementById("demo3");
 
+function text123() {
+x.innerHTML = "Hello Guys";
+y.innerText = "What you Say";
+z.style.color = "red";
+}
+
+setTimeout(text123, 5000);
